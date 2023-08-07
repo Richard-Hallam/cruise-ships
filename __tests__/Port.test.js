e08b8e9d905f08eb48c72f102d5a8ad1eeadc992
Richard Hallam <richardhallam25@gmail.com>
2023-08-07 08:37:39 +0100

    Added removeShip() to port.js and created tests for it

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,4 +1,5 @@
 /*globals describe it expect*/
+const Itinerary = require('../src/Itinerary.js');
 const Port = require('../src/Port.js');
 
 describe('Port', () => {
@@ -6,4 +7,26 @@ describe('Port', () => {
     it('Checks Port is an object', () => {
         expect(new Port()).toBeInstanceOf(Object);
     })
+
+    it('Checks Port can have ships', () => {
+        const port = new Port('Mousehole');
+        const itinerary = new Itinerary(port);
+        const ship = itinerary;
+
+        port.addShip(ship);
+        expect(port.ships).toContain(ship)
+    })
+
+    it('can remove a ship', () => {
+        const port = new Port('Mousehole');
+        const scilionian = {};
+        const kipper = {};
+
+        port.addShip(scilionian);
+        port.addShip(kipper);
+        port.removeShip(kipper);
+
+        expect(port.ships).toEqual([scilionian]);
+    })
 })
+
--- src/Port.js
+++ src/Port.js
@@ -1,5 +1,14 @@
 function Port(portName){
     this.name = portName;
+    this.ships = [];
+
+    this.addShip = function(ship){
+        this.ships.push(ship);
+    }
+
+    this.removeShip = function(ship){
+        this.ships = this.ships.filter(item => item !== ship);
+    }
 }
 
 module.exports = Port;
