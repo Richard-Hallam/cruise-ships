cdfc8b3d720030c073dc97ed6c553b673f5fc431
Richard Hallam <richardhallam25@gmail.com>
2023-08-30 20:02:21 +0100

    Refactored port tests to make them DRY

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -3,30 +3,36 @@ const Itinerary = require('../src/Itinerary.js');
 const Port = require('../src/Port.js');
 
 describe('Port', () => {
+    describe("with ports and itinarary", () => {
+        let port;
+        let itinarary;
+        let ship;
+        let scilionian;
+        let kipper;
+        beforeEach(() =>{
+            port = new Port('Mousehole');
+            itinerary = new Itinerary(port);
+            ship = itinerary;
+            scilionian = {};
+            kipper = {};
+        })
+        it('Checks Port is an object', () => {
+            expect(new Port()).toBeInstanceOf(Object);
+        })
 
-    it('Checks Port is an object', () => {
-        expect(new Port()).toBeInstanceOf(Object);
-    })
+        it('Checks Port can have ships', () => {
+            port.addShip(ship);
+            expect(port.ships).toContain(ship)
+        })
 
-    it('Checks Port can have ships', () => {
-        const port = new Port('Mousehole');
-        const itinerary = new Itinerary(port);
-        const ship = itinerary;
+        it('can remove a ship', () => {
 
-        port.addShip(ship);
-        expect(port.ships).toContain(ship)
-    })
+            port.addShip(scilionian);
+            port.addShip(kipper);
+            port.removeShip(kipper);
 
-    it('can remove a ship', () => {
-        const port = new Port('Mousehole');
-        const scilionian = {};
-        const kipper = {};
-
-        port.addShip(scilionian);
-        port.addShip(kipper);
-        port.removeShip(kipper);
-
-        expect(port.ships).toEqual([scilionian]);
-    })
-})
+            expect(port.ships).toEqual([scilionian]);
+        })
+});
+});
 
